853a7182433a531bd7a39b519e374d41f9f42fad
Alan Inglis <alan.n.inglis@gmail.com>
2020-02-03 01:11:29 +0000

    Updated code

--- NetworkPlot.R
+++ NetworkPlot.R
@@ -70,26 +70,18 @@ frMod <- train(FR.regr.lrn, frRgrTask)
 # Graph Function ----------------------------------------------------------
 
 
-importanceNet <- function(task, model, method = "randomForest",Threshold = F, Cluster = F,...){
+importanceNet <- function(task, model, method = "randomForest_importance", thresholdValue = 0, 
+                          cluster = F,...){
   
   
   # get data:
   data <- getTaskData(task)
   
-  # Get importance:
+  # Get importance values:
   
-  norm.task <- normalizeFeatures(task, method = "standardize")
-  if(method == "randomForest"){
-    impMethod = c("randomForest_importance")}
-  else if(method == "ranger Permutation"){
-    impMethod = c("ranger_permutation")}
-  else if(method == "rfSRC Importance"){
-    impMethod = c("randomForestSRC_importance")
-  }
-  else if(method >=4)
-    (return("Invalid method chosen. Use ?intHeatmap for allowed methods"))
-  im_feat <- generateFilterValuesData(norm.task, method = impMethod)
-  Imp <- im_feat$data$value
+  normTask <- normalizeFeatures(task, method = "standardize")
+  impFeat <- generateFilterValuesData(normTask, method = method)
+  Imp <- impFeat$data$value
   Imp <- (100)*(Imp-min(Imp))/(max(Imp)-min(Imp))  # Scale the Importance between 10-50 
   
   Mod <- Predictor$new(model, data = data) # iml Interaction Strength
@@ -108,67 +100,104 @@ importanceNet <- function(task, model, method = "randomForest",Threshold = F, Cl
   dinteraction[vars2] <- res[[".interaction"]]                              # set values   
   dinteraction <- (dinteraction+t(dinteraction))/2                          # avg over values to make symmetrical
   
+  
+  
   dinteraction1 <- data.frame(interaction=as.vector(dinteraction))   
   
-  # setting var names:
-  INT <- dinteraction1
-  UINT <- unique(INT)
-  UINT <- UINT[-1,]
-  IMP <-  Imp
-  nam <- getTaskFeatureNames(task)
+  sortInt = t(dinteraction)[lower.tri(t(dinteraction), diag=FALSE)]  # get upper triangle of the matrix by row order
+  sorted_Int <- sort(sortInt, index.return=TRUE)                     # Sort values whilst preserving the index 
+  Int <- sorted_Int$x
+  nam <- getTaskFeatureNames(task)                                   # Get feature names 
   
-  # # Set path direction of graph:
+  
+  # Set path direction of graph:
   g <- sample_pa(length(nam), m = length(nam))
   df <- igraph::as_data_frame(g)
   gDF <- dplyr::arrange(df, to)
   gDFL <- rbind(gDF$from,gDF$to)
-
-  # Graph Parameters:
+  matched_gDFL <- gDFL[,sorted_Int$ix]
   
-  net.bg <- make_graph(gDFL, length(nam)) 
-
-  RImp <- round(Imp, 2)
-  RImpFactor <- as.factor(RImp)
-  UintScaled <- UINT *20
-  Rint <- round(UintScaled, 2)
-  colfunc <- colorRampPalette(c("black", "red"))
-  colfuncNode <- colorRampPalette(c("blue", "red"))
+  # Create network graph:
+  net.bg <- make_graph(matched_gDFL, length(nam)) 
   
-  #highlight max interaction strength
-  maxWeight <- max(UINT*100)
- 
+  # Scale and round values:
+  Int <- round(Int*10,2)
+  E(net.bg)$weight <- Int
+  Imp  <- round(Imp,2)
   
-  if(Threshold == FALSE && Cluster == FALSE){
-    p <- ggnet2(net.bg, mode = "circle", 
-              size = RImp,
-              node.color = "grey50",
-              label = nam,
-              edge.size = UintScaled,
-              edge.label = Rint,
-              edge.color = colfunc(length(Rint)))+
-       theme(legend.text = element_text(size = 0))
-    return(p) }else if(Threshold == TRUE && Cluster == FALSE){
-      a <- sort(Rint, decreasing  = TRUE)
-      cut.off <- a[1:5]
-      net.sp  <- delete_edges(net.bg, E(net.bg)[Rint<cut.off])
-      pp <- ggnet2(net.sp, mode = "circle",
-                  size = RImp,
-                  color = "grey50",
-                  node.color = "grey50",
-                  label = nam,
-                  edge.size = Rint)+
-        theme(legend.text = element_text(size = 0))
-                  #edge.label = Rint)
-                  #edge.color = colfunc(length(Rint)))
-      return(pp)}
+  
+  # Set colour palette: 
+  colFunc <- colorRampPalette(c("black", "red"))
+  
+  # Plotting function:
+  # if(cluster == FALSE){
+  #   p <- ggnet2(net.bg, mode = "circle", 
+  #             size = Imp,
+  #             node.color = "grey80",
+  #             label = nam,
+  #             edge.size = Rint,
+  #             edge.label = Rint,
+  #            edge.color = colfunc(length(Rint)))+
+  #      theme(legend.text = element_text(size = 0))
+  #   return(p) }
+  #}else 
+  if(cluster == FALSE){
+    a <- sort(Int, decreasing  = TRUE)
+    idx <- which(a > thresholdValue)
+    cut.off <- a[1:max(idx)]
+    `%notin%` <- Negate(`%in%`)
+    net.sp  <- delete_edges(net.bg, E(net.bg)[E(net.bg)$weight %notin% cut.off])
+    weightDF <- get.data.frame(net.sp) # get df of graph attributes
+    edgeWidth1 <- weightDF$weight  # select edge weight
+    pp <- ggnet2(net.sp, mode = "circle",
+                 size = Imp,
+                 color = "grey80",
+                 label = nam,
+                 edge.size = edgeWidth1,
+                 edge.label = edgeWidth1,
+                 edge.color = colFunc(length(edgeWidth1)))+
+      theme(legend.text = element_text(size = 0))
+    return(pp)}
+  # }else if(Threshold == FALSE && Cluster == TRUE){
+  # V(net.bg)$label <- nam
+  # E(net.bg)$width <- Rint
+  # E(net.bg)$arrow.mode <- 0
+  # E(net.bg)$label <- Rint
+  # E(net.bg)$label.cex <- 1
+  # clp <- cluster_optimal(net.bg)
+  #ppp <- plot(clp, net.bg, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")
+  # weightDF <- get.data.frame(net.bg) # get df of graph attributes
+  # EdgeWidth2 <- weightDF$weight 
+  # clp <- cluster_optimal(net.bg)
+  # 
+  # ppp <- ggnet2(clp)
+  # , mode = "circle",
+  #            size = Imp,
+  #            color = "grey50",
+  #            node.color = "grey50")
+  #              label = nam,
+  #              edge.size = Rint,
+  #              edge.label = Rint,
+  #    edge.color = colfunc(length(Rint)))+
+  # theme(legend.text = element_text(size = 0))
+  #  return(ppp)}
+  # }else if(Threshold == TRUE && Cluster == TRUE){
+  #   V(net.bg)$label <- nam
+  #   E(net.bg)$width <- Rint
+  #   E(net.bg)$arrow.mode <- 0
+  #   a <- sort(UINT, decreasing  = TRUE)
+  #   cut.off <- a[1:5]
+  #   net.sp  <- delete_edges(net.bg, E(net.bg)[UINT<cut.off])
+  #   clp <- cluster_optimal(net.sp)
+  #   pppp <- plot(clp, net.sp, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")}
 }
 
 set.seed(1701)
-importanceNet(aqRgrTask, aqMod, method = "randomForest", Threshold = F, Cluster = F)
+importanceNet(aqRgrTask, aqMod, method = "randomForest_importance", thresholdValue = 0.99, cluster = F)
 
-importanceNet(irClasTask, irMod, method = "rfSRC Importance")
+importanceNet(irClasTask, irMod)
 
-importanceNet(frRgrTask, frMod, method = "ranger Permutation", Threshold = F, Cluster = F) 
+importanceNet(frRgrTask, frMod, method = "ranger_impurity", Threshold = F, Cluster = F) 
 
 
 
--- NewHeatmap.R
+++ NewHeatmap.R
@@ -6,9 +6,6 @@ library(mlr)
 library(iml)
 library(plotly)
 library(reshape2)
-library(randomForest)
-library(randomForestSRC)
-library(ranger)
 
 set.seed(1701)
 
@@ -66,18 +63,22 @@ frMod <- train(FR.regr.lrn, frRgrTask)
 ## Heatmap Plotting Function -------------------------------------------------------
 
 intHeatmap <- function(task, model, method = "randomForest",
-                       interact = FALSE,...){
+                       interactive = FALSE, intLow = "floralwhite", intHigh = "dodgerblue4",
+                       impLow = "white", impHigh = "firebrick1",...){
   # get data:
   data <- getTaskData(task)
+  
   # Get Importance Measures -------------------------------------------------
   
-  norm.task <- normalizeFeatures(task, method = "standardize")
+  normTask <- normalizeFeatures(task, method = "standardize")
   
-  im_feat <- generateFilterValuesData(norm.task, method = method)
-  Y_Imp <- im_feat$data$value
-
-
-
+  impFeat <- generateFilterValuesData(normTask, method = method)
+  yImp <- impFeat$data$value
+  
+  
+  nam <- getTaskFeatureNames(task)
+  
+  
   mod  <- Predictor$new(model, data)
   res  <- NULL
   ovars <- getTaskFeatureNames(task)
@@ -91,11 +92,29 @@ intHeatmap <- function(task, model, method = "randomForest",
   rownames(dinteraction) <- colnames(dinteraction) <- ovars                 # set names
   dinteraction[vars2] <- res[[".interaction"]]                              # set values   
   dinteraction <- (dinteraction+t(dinteraction))/2                          # avg over values to make symmetrical
-  diag(dinteraction) <- Y_Imp
   
-  nam <- getTaskFeatureNames(task)
-# Now create a plot -------------------------------------------------------
-
+  # Sort matrix so max value is top left
+  q <- which(colSums(dinteraction == max(dinteraction))>0,arr.ind = T)
+  o <- c(q, seq(ncol(dinteraction))[-q])
+  dinteraction <- dinteraction[o,o]
+  
+  # Reorder the variable importance on diag to match the new order of plot:
+  labelNames <- colnames(dinteraction) 
+  yImpName <- list(nam)
+  matV <- matrix(yImp, dimnames = yImpName)
+  matV <- t(matV)
+  
+  matV <- matV[, labelNames, drop = FALSE]
+  yImp <- matV[1,]
+  
+  diag(dinteraction) <- yImp
+  
+  labTitle = method
+  
+  
+  
+  # Now create a plot -------------------------------------------------------
+  
   var_int2 = dinteraction %>% as_tibble %>% 
     mutate(var_num1 = 1:length(nam)) %>% 
     pivot_longer(cols = 1:length(nam),
@@ -105,10 +124,30 @@ intHeatmap <- function(task, model, method = "randomForest",
            alpha_int = 1 - alpha_imp,
            `Variable\nImportance` = alpha_imp*`Interaction\nStrength`,
            `Interaction\nStrength` = alpha_int*`Interaction\nStrength`)
-
   
   
-
+  # Create Plot: ------------------------------------------------------------
+  
+  
+  p <- ggplot(data = var_int2, 
+              mapping = aes(x = var_num1, y = var_num2)) + 
+    scale_x_continuous(breaks = 1:length(nam), labels = labelNames, position = "top") + 
+    scale_y_reverse(breaks = 1:length(nam), labels = labelNames) +
+    geom_raster(aes(fill = `Interaction\nStrength`),
+                alpha = var_int2$alpha_int) + 
+    scale_fill_gradient(low = intLow, high = intHigh) +
+    new_scale_fill() +
+    geom_raster(aes(fill = `Variable\nImportance`),
+                alpha = var_int2$alpha_imp) + 
+    scale_fill_gradient(low = impLow ,high = impHigh) +
+    ggtitle(labTitle) +
+    xlab('') + 
+    ylab('') + 
+    theme_light() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
+  
+  
   # for plotly  -------------------------------------------------------------
   dinteraction1 <- data.frame(interaction=as.vector(dinteraction))          # Create df
   
@@ -120,42 +159,25 @@ intHeatmap <- function(task, model, method = "randomForest",
   dinteraction1$vary <- factor(dinteraction1$vary, levels=rev(ovars))      
   
   # Importance point size:
-  if(length(Y_Imp) <= 5)
+  if(length(yImp) <= 5)
   {pointSize = 15
-  } else if(length(Y_Imp) <= 10)
+  } else if(length(yImp) <= 10)
   {pointSize = 10
-  }else if(length(Y_Imp <= 15))
+  }else if(length(yImp <= 15))
   {pointSize = 7}
   
-  labTitle = method
-  
-  impMat <- matrix(0,nrow=length(Y_Imp), ncol=length(Y_Imp))
-  diag(impMat) <- Y_Imp
+  impMat <- matrix(0,nrow=length(yImp), ncol=length(yImp))
+  diag(impMat) <- yImp
   importMatrix <- melt(impMat)
   
-  p <- ggplot(data = var_int2, 
-         mapping = aes(x = var_num1, y = var_num2)) + 
-    scale_x_continuous(breaks = 1:length(nam), labels = nam, position = "top") + 
-    scale_y_reverse(breaks = 1:length(nam), labels = nam) +
-    geom_raster(aes(fill = `Interaction\nStrength`),
-                alpha = var_int2$alpha_int) + 
-    new_scale_fill() +
-    geom_raster(aes(fill = `Variable\nImportance`),
-                alpha = var_int2$alpha_imp) + 
-    scale_fill_gradient(low = "red4",high = "red") +
-    ggtitle(labTitle) +
-    xlab('') + 
-    ylab('') + 
-    theme_light() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank())
   
+  # Interactive Plot --------------------------------------------------------
   # This plot is only called for plotly as newGeom_raster is not supported
-  Importance <- Y_Imp
+  Importance <- yImp
   pp <- ggplot(data=dinteraction1, aes(x=varx,y=vary)) + 
     geom_tile(aes(fill = interaction), dinteraction1 %>% filter(dinteraction1$varx != dinteraction1$vary)) +
     geom_point(aes(x=Var1,y= rev(Var2),colour = Importance),size = pointSize, importMatrix %>% filter(importMatrix$Var1==importMatrix$Var2)) +
-    scale_colour_gradient(low="red4", high="red") +
+    scale_fill_gradient2(low="white", high="red") +
     labs(colour= labTitle) +
     labs(fill='Interaction \n\ Strength') +
     theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
@@ -165,12 +187,15 @@ intHeatmap <- function(task, model, method = "randomForest",
   
   # Interactive plot using plotly
   ppp <- ggplotly(pp, tooltip = "all")
-
-  if(interact == TRUE){
+  
+  if(interactive == TRUE){
     return(ppp)
   }else{return(p)}
 }
   
 intHeatmap(aqRgrTask, aqMod, method = "randomForest_importance", interact = F)
+intHeatmap(aqRgrTask, aqMod, method = "randomForest_importance", interactive = F,
+           intLow = "yellow", intHigh = "dodgerblue4", impLow = "white", impHigh = "black")
+
 intHeatmap(irClasTask, irMod, method = "ranger_permutation", interact = F)
 intHeatmap(frRgrTask, frMod, method = "randomForestSRC_importance", interact = F)
