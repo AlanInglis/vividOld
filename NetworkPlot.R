a89a3bae4051380bc1021ffd234832afd421b510
Alan Inglis <alan.n.inglis@gmail.com>
2020-02-19 12:32:18 +0000

    networkPlot update

--- NetworkPlot.R
+++ NetworkPlot.R
@@ -14,7 +14,11 @@ library(RColorBrewer)
 # Graph Function ----------------------------------------------------------
 
 
+# Graph Function ----------------------------------------------------------
+
+
 importanceNet <- function(task, model, method = "randomForest_importance", thresholdValue = 0,
+                          label = T, minInt = 0, maxInt = 1, minImp = 0, maxImp = impLegend,
                           cluster = F,...){
 
 
@@ -26,7 +30,12 @@ importanceNet <- function(task, model, method = "randomForest_importance", thres
   normTask <- normalizeFeatures(task, method = "standardize")
   impFeat <- generateFilterValuesData(normTask, method = method)
   Imp <- impFeat$data$value
-  Imp <- (100)*(Imp-min(Imp))/(max(Imp)-min(Imp))  # Scale the Importance between 10-50
+  impWarn <- impFeat$data$value
+  impWarn <- max(impWarn)
+  impLegend <- impFeat$data$value
+  impLegend <- round(impLegend, 2)
+  impLegend <- max(impLegend)
+  #Imp <- (100)*(Imp-min(Imp))/(max(Imp)-min(Imp))  # Scale the Importance between 0-100
 
   Mod <- Predictor$new(model, data = data) # iml Interaction Strength
 
@@ -65,96 +74,127 @@ importanceNet <- function(task, model, method = "randomForest_importance", thres
   net.bg <- make_graph(matched_gDFL, length(nam))
 
   # Scale and round values:
-  Int <- round(Int*10,2)
+  Int <- round(Int,2)
   E(net.bg)$weight <- Int
+  Imp <- (5-1)*((Imp-min(Imp))/(max(Imp)-min(Imp)))+1 # scale between 1-5
   Imp  <- round(Imp,2)
 
 
   # Set colour palette:
   colFunc <- colorRampPalette(c("floralwhite", "dodgerblue4"))
 
- # Plotting function:
-  # if(cluster == FALSE){
-  #   p <- ggnet2(net.bg, mode = "circle",
-  #             size = Imp,
-  #             node.color = "grey80",
-  #             label = nam,
-  #             edge.size = Rint,
-  #             edge.label = Rint,
-  #            edge.color = colfunc(length(Rint)))+
-  #      theme(legend.text = element_text(size = 0))
-  #   return(p) }
-     #}else
-       if(cluster == FALSE){
-       a <- sort(Int, decreasing  = TRUE)
-       idx <- which(a > thresholdValue)
-       cut.off <- a[1:max(idx)]
-       `%notin%` <- Negate(`%in%`)
-       net.sp  <- delete_edges(net.bg, E(net.bg)[E(net.bg)$weight %notin% cut.off])
-       weightDF <- get.data.frame(net.sp) # get df of graph attributes
-       edgeWidth1 <- weightDF$weight  # select edge weight
-       pp <- ggnet2(net.sp, mode = "circle",
-                   size = Imp,
-                   color = "red",
-                   #label = nam,
-                   edge.size = edgeWidth1,
-                   edge.label = edgeWidth1,
-                   edge.color = colFunc(length(edgeWidth1)))+
-         theme(legend.text = element_text(size = 0))+
-         geom_label(aes(label = nam),
-                          nudge_y = 0.07,
-                   )
-       return(pp)}
-     # }else if(Threshold == FALSE && Cluster == TRUE){
-       # V(net.bg)$label <- nam
-       # E(net.bg)$width <- Rint
-       # E(net.bg)$arrow.mode <- 0
-       # E(net.bg)$label <- Rint
-       # E(net.bg)$label.cex <- 1
-       # clp <- cluster_optimal(net.bg)
-       #ppp <- plot(clp, net.bg, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")
-       # weightDF <- get.data.frame(net.bg) # get df of graph attributes
-       # EdgeWidth2 <- weightDF$weight
-       # clp <- cluster_optimal(net.bg)
-       #
-       # ppp <- ggnet2(clp)
-       # , mode = "circle",
-       #            size = Imp,
-       #            color = "grey50",
-       #            node.color = "grey50")
-     #              label = nam,
-     #              edge.size = Rint,
-     #              edge.label = Rint,
-     #    edge.color = colfunc(length(Rint)))+
-     # theme(legend.text = element_text(size = 0))
-     #  return(ppp)}
-     # }else if(Threshold == TRUE && Cluster == TRUE){
-     #   V(net.bg)$label <- nam
-     #   E(net.bg)$width <- Rint
-     #   E(net.bg)$arrow.mode <- 0
-     #   a <- sort(UINT, decreasing  = TRUE)
-     #   cut.off <- a[1:5]
-     #   net.sp  <- delete_edges(net.bg, E(net.bg)[UINT<cut.off])
-     #   clp <- cluster_optimal(net.sp)
-     #   pppp <- plot(clp, net.sp, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")}
+  # Set shape of plot:
+  if(thresholdValue > 0){
+    l <- layout.fruchterman.reingold(net.bg)
+  }else{l <- "circle"}
+
+  # Set up plot paramters:
+  colfunction <- colorRampPalette(c("floralwhite", "firebrick1"))
+  nodeCol <- colfunction(length(nam))
+  a <- sort(Int, decreasing  = TRUE)
+  idx <- which(a > thresholdValue)
+  cut.off <- a[1:max(idx)]
+  `%notin%` <- Negate(`%in%`)
+  net.sp  <- delete_edges(net.bg, E(net.bg)[E(net.bg)$weight %notin% cut.off])
+  weightDF <- get.data.frame(net.sp) # get df of graph attributes
+  edgeWidth1 <- weightDF$weight  # select edge weight
+  edgeWidth2 <- (5-1)*((edgeWidth1-min(edgeWidth1))/(max(edgeWidth1)-min(edgeWidth1)))+1 # scale between 1-5
+
+  # Warning for max/min Int/Imp:
+  if(maxInt > 1){
+    warning("Legend value selected for Interaction Strength (i.e., maxInt) is larger than maximum interaction value")
+  }
+  if(maxImp > (impWarn+1)){
+    warning("Legend value selected for Variable Importance (i.e., maxImp) is larger than maximum importance value")
+  }
+  if(minImp < 0){
+    stop("minImp must not be less than zero")
+  }
+  if(minInt < 0){
+    stop("minInt must not be less than zero")
+  }
+  if(minImp > (impWarn+1)){
+    stop("minImp is lager than the maximum importance value")
+  }
+  if(minInt > 1){
+    stop("minInt must not be greater than 1")
+  }
+
+
+  # Plotting function -------------------------------------------------------
+  if(label == TRUE){
+    p <- ggnet2(net.sp, mode = l,
+                size = 0,
+                #color = "red",
+                edge.size = edgeWidth2,
+                edge.label = edgeWidth1,
+                edge.color = colFunc(length(edgeWidth1))) +
+      theme(legend.text = element_text(size = 10)) +
+      geom_label(aes(label = nam),nudge_y = 0.07) +
+      new_scale_fill() +
+      geom_point(aes(fill = Imp), size = Imp*2, col = nodeCol) +
+      scale_fill_continuous(name = "Interaction\nStrength",
+                            limits=c(minInt, maxInt), breaks=seq(minInt, maxInt, by= 0.25),
+                            low = "floralwhite" ,high = "dodgerblue4") +
+      new_scale_fill() +
+      geom_point(aes(fill = Imp), size = 0, col = nodeCol) +
+      scale_fill_continuous(name = "Variable\nImportance",
+                            limits=c(minImp, maxImp), breaks=seq(minImp, maxImp, by= 100),
+                            low = "floralwhite" ,high = "firebrick1")
+    return(p)
+  }else if(label == FALSE){
+    p <- ggnet2(net.sp, mode = l,
+                size = 0,
+                #color = "red",
+                edge.size = edgeWidth2,
+                #edge.label = edgeWidth1,
+                edge.color = colFunc(length(edgeWidth1))) +
+      theme(legend.text = element_text(size = 10)) +
+      geom_label(aes(label = nam),nudge_y = 0.07) +
+      new_scale_fill() +
+      geom_point(aes(fill = Imp), size = Imp*2, col = nodeCol) +
+      scale_fill_continuous(name = "Interaction\nStrength",
+                            limits=c(minInt, maxInt), breaks=seq(minInt, maxInt, by= 0.25),
+                            low = "floralwhite" ,high = "dodgerblue4") +
+      new_scale_fill() +
+      geom_point(aes(fill = Imp), size = 0, col = nodeCol) +
+      scale_fill_continuous(name = "Variable\nImportance",
+                            limits=c(minImp, maxImp), breaks=seq(minImp, maxImp, by= 100),
+                            low = "floralwhite" ,high = "firebrick1")
+    return(p)}
+  # }else if(Threshold == FALSE && Cluster == TRUE){
+  # V(net.bg)$label <- nam
+  # E(net.bg)$width <- Rint
+  # E(net.bg)$arrow.mode <- 0
+  # E(net.bg)$label <- Rint
+  # E(net.bg)$label.cex <- 1
+  # clp <- cluster_optimal(net.bg)
+  #ppp <- plot(clp, net.bg, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")
+  # weightDF <- get.data.frame(net.bg) # get df of graph attributes
+  # EdgeWidth2 <- weightDF$weight
+  # clp <- cluster_optimal(net.bg)
+  #
+  # ppp <- ggnet2(clp)
+  # , mode = "circle",
+  #            size = Imp,
+  #            color = "grey50",
+  #            node.color = "grey50")
+  #              label = nam,
+  #              edge.size = Rint,
+  #              edge.label = Rint,
+  #    edge.color = colfunc(length(Rint)))+
+  # theme(legend.text = element_text(size = 0))
+  #  return(ppp)}
+  # }else if(Threshold == TRUE && Cluster == TRUE){
+  #   V(net.bg)$label <- nam
+  #   E(net.bg)$width <- Rint
+  #   E(net.bg)$arrow.mode <- 0
+  #   a <- sort(UINT, decreasing  = TRUE)
+  #   cut.off <- a[1:5]
+  #   net.sp  <- delete_edges(net.bg, E(net.bg)[UINT<cut.off])
+  #   clp <- cluster_optimal(net.sp)
+  #   pppp <- plot(clp, net.sp, vertex.label.family = "Helvetica", edge.label.family = "Helvetica")}
 }
 
-# Get data ----------------------------------------------------------------
-## Air quality data (used for regression)
-
-aq <- data.frame(airquality)
-aq <- na.omit(aq)
-
-# mlr set up --------------------------------------------------------------
-
-aqRgrTask  <- makeRegrTask(data = aq, target = "Ozone")
-aqRegrLrn <- makeLearner("regr.randomForest")
-aqMod <- train(aqRegrLrn, aqRgrTask)
-
-
-importanceNet(aqRgrTask, aqMod, method = "randomForest_importance", thresholdValue = 0, cluster = F)
-
-# With Thresholding
-importanceNet(aqRgrTask, aqMod, method = "randomForest_importance", thresholdValue = 0.99, cluster = F)
 
 
