4a659c8346c08a2c9f96cc2d0f09208fb663d391
Alan Inglis <alan.n.inglis@gmail.com>
2020-05-10 22:27:06 +0100

    Diagonal now shows pdp for the predictor

--- R/ggpdpPairs.R
+++ R/ggpdpPairs.R
@@ -46,8 +46,10 @@
 
 ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", colHigh = "#56B1F7",
                        fitlims = NULL,gridsize = 10,class=1,cardinality = 20, ...){
-  prob <- model$learner$type == "classif"
+
+   prob <- model$learner$type == "classif"
   data <- getTaskData(task)
+
   # make iml model
   if (prob){
     pred.data <- Predictor$new(model, data = data, class=class)
@@ -56,6 +58,24 @@ ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", 
     pred.data <- Predictor$new(model, data = data)
   }
 
+  # Get data for individual variables
+  xdata <- pred.data$data$get.x()
+  if (!is.null(vars) & all(vars %in% names(xdata)))
+    xdata <- xdata[,vars]
+  xvar1 <- expand.grid(1:ncol(xdata), 1:ncol(xdata))[,2:1]
+  xvar <- xvar1$Var1
+  xvar <- as.matrix(xvar)
+  xvarn <- cbind(names(xdata)[xvar[,1]])
+  xvarn <- xvarn[1:length(unique(xvarn))]
+  xvarn <- as.matrix(xvarn)
+
+  # loop through vars and create a list of pdps
+  pdplist1 <- vector("list", length=nrow(xvarn))
+  for (i in 1:nrow(xvarn))
+    pdplist1[[i]] <-FeatureEffect$new(pred.data, xvarn[i,], method = "pdp+ice", grid.size=10)
+  names(pdplist1)  <- paste(xvarn[,1])
+
+  # Get data for pairs of variables
   xdata <- pred.data$data$get.x()
   if (!is.null(vars) & all(vars %in% names(xdata)))
     xdata <- xdata[,vars]
@@ -63,11 +83,14 @@ ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", 
   xyvar <- as.matrix(xyvar[xyvar[,1]<xyvar[,2],])
   xyvarn <- cbind(names(xdata)[xyvar[,1]], names(xdata)[xyvar[,2]])
 
+
+  # loop through vars and create a list of pdps for each pair
   pdplist <- vector("list", length=nrow(xyvarn))
   for (i in 1:nrow(xyvarn))
     pdplist[[i]] <-FeatureEffect$new(pred.data, xyvarn[i,], method = method, grid.size=gridsize)
   names(pdplist)  <- paste(xyvarn[,1], xyvarn[,2], sep="pp")
 
+   # Set limits for pairs
   if (is.null(fitlims)){
     r <- sapply(pdplist, function(x) range(x$results[,3]))
     r <- range(r)
@@ -75,6 +98,7 @@ ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", 
   } else
     limits <- fitlims
 
+  # Plot prep for pairs
   ggpdp <- function(data, mapping, ...) {
     vars <- c(quo_name(mapping$x), quo_name(mapping$y))
     pdp <- pdplist[[paste(vars[2], vars[1], sep="pp")]]
@@ -82,6 +106,13 @@ ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", 
     plot(pdp, rug=FALSE) + scale_fill_gradient(name="yhat",low=colLow, high=colHigh,limits=limits)
   }
 
+  # Plot prep for diag pdps
+  ggpdpDiag <- function(data, mapping, ...) {
+    vars <- c(quo_name(mapping$x), quo_name(mapping$y))
+    pdp <- pdplist1[[paste(vars[1])]]
+    plot(pdp, rug=FALSE)}
+
+  # plot prep for class.
   ggpdpc <- function(data, mapping, ...) {
     vars <- c(quo_name(mapping$x), quo_name(mapping$y))
     pdp <- pdplist[[paste(vars[2], vars[1], sep="pp")]]
@@ -98,11 +129,12 @@ ggpdpPairs <- function(task, model, method="pdp",vars=NULL, colLow = "#132B43", 
     else w <- NULL
   }
   p <- ggpairs(xdata,
-               upper=list(
-                 continuous = ggpdp, combo=ggpdpc, discrete=ggpdp),
-                lower=list(continuous=wrap("points", size=.2)), legend=w,
+               upper=list(continuous = ggpdp, combo=ggpdpc, discrete=ggpdp),
+               diag = list(continuous = ggpdpDiag),
+               lower=list(continuous=wrap("points", size=.2)), legend=w,
                cardinality_threshold = cardinality)
   suppressMessages(print(p))
   invisible(p)
 
 }
+
