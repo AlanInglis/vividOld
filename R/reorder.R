87f188c6449e8bdfca8eda6d29e8b4d13b93fa91
Alan Inglis <alan.n.inglis@gmail.com>
2021-03-03 16:32:37 +0000

    reformatted

--- R/reorder.R
+++ R/reorder.R
@@ -30,27 +30,24 @@
 #' myMat <- vividMatrix(task = Task, model = Mod, gridsize = 100, reorder = FALSE)
 #'
 #' myMat <- reorderMatrix(myMat)
-#'
 #' @importFrom DendSer "dser"
 #'
 #' @export
 
-reorderMatrix <- function(d){
+reorderMatrix <- function(d) {
   vimp <- diag(d)
   rvimp <- range(vimp)
-  if (rvimp[2] != rvimp[1])
-    vimp <- (vimp - rvimp[1])/(rvimp[2] - rvimp[1])
+  if (rvimp[2] != rvimp[1]) {
+    vimp <- (vimp - rvimp[1]) / (rvimp[2] - rvimp[1])
+  }
   vint <- as.dist(d)
   rvint <- range(vint)
-  if (rvint[2] != rvint[1])
-    vint <-  (vint - rvint[1])/(rvint[2] - rvint[1])
-  score <- apply(as.matrix(vint), 1,max) + vimp
-  o <- dser( -vint , -score, cost=costLS)
-  res<- d[o,o]
-  class(res)<- c("vivid", "matrix", "array")
+  if (rvint[2] != rvint[1]) {
+    vint <- (vint - rvint[1]) / (rvint[2] - rvint[1])
+  }
+  score <- apply(as.matrix(vint), 1, max) + vimp
+  o <- dser(-vint, -score, cost = costLS)
+  res <- d[o, o]
+  class(res) <- c("vivid", "matrix", "array")
   res
 }
-
-
-
-
